980cd137746f12f722001f980258895b7660a1c8
yrdgl <ozge.melis@hotmail.com>
2026-01-09 15:57:17 +0300

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import HomePage from './pages/HomePage';
-import JournalPage from './pages/DailyJournals';
+// JournalPage import'u SİLİNDİ
 
 function App() {
   return (
@@ -12,7 +12,7 @@ function App() {
         <Header />
         <Routes>
           <Route path="/" element={<HomePage />} />
-          <Route path="/candemirin-ay-gunlugu" element={<JournalPage />} />
+          {/* JournalPage route'u SİLİNDİ */}
         </Routes>
       </BrowserRouter>
     </div>
